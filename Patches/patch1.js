813f2fee43828c315b239cdcd6b8415d0bf95aaf
Super-Programmer-Navi <navdip.dhillon.91@gmail.com>
2021-04-11 14:31:53 -0700

    First Commit

---
+++ Patches/patch1.js
@@ -0,0 +1,6 @@
+// patches should have a few things in here
+
+// should require some sort of pre-req, said patches should be done first then this file can be invoked
+
+// here should be an SQL transaction that when pre reqs have been completed to
+// then would assign to SQL string variable and passed to server/middleware to then perform patch 
---
+++ index.js
@@ -0,0 +1,4 @@
+// need to set up server and then make connection with pg
+
+
+
